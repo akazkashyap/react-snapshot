7b0b52cfc97cc5b4bc152db2482e60cac2c86732
Akash <52403079+akazkashyap@users.noreply.github.com>
2026-01-21 18:09:10 +0530

    Update API key in mainApi.js
    Replaced API key with a hardcoded value for testing.

--- src/Api/mainApi.js
+++ src/Api/mainApi.js
@@ -6,7 +6,8 @@ import axios from 'axios'
 export default axios.create({
     baseURL: "https://pixabay.com/api/",
     params: {
-        key: process.env.REACT_APP_API_KEY,
+        // key: process.env.REACT_APP_API_KEY,
+        key: "34161620-bc5bb1c6eaff0b33298d205b0",
         per_page: 48
     }
 })
