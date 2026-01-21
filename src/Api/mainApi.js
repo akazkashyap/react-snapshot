57d917211b3794d1fa1c1c28b303a6c23cba4da4
Akash <52403079+akazkashyap@users.noreply.github.com>
2026-01-21 16:22:32 +0530

    Replace hardcoded API key with environment variable

--- src/Api/mainApi.js
+++ src/Api/mainApi.js
@@ -6,8 +6,7 @@ import axios from 'axios'
 export default axios.create({
     baseURL: "https://pixabay.com/api/",
     params: {
-        //key: process.env.REACT_APP_API_KEY,
-        key: "34161620-bc5bb1c6eaff0b33298d205b0",
+        key: process.env.REACT_APP_API_KEY,
         per_page: 48
     }
 })
